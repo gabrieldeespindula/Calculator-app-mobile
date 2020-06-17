ead2ab0273abb35d538d3607a0a25b9b1c73cc57
Gabriel de Espindula <gabrieldeespindula80@gmail.com>
2020-06-17 14:15:27 -0300

    Creating menu, configuring it.

--- app/src/main/java/com/gabrieldeespindula/calculator/MainActivity.java
+++ app/src/main/java/com/gabrieldeespindula/calculator/MainActivity.java
@@ -2,11 +2,33 @@ package com.gabrieldeespindula.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                if (item.isChecked()){
+                    item.setChecked(false);
+                }
+                else {
+                    item.setChecked(true);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
